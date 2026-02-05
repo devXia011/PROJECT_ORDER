e57484cdd093aeb23f221d19965d5ab011bcf930
devXia011 <xmadera1992@gmail.com>
2026-02-05 12:19:21 +0800

    cache

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,6 +9,27 @@ app = FastAPI()
 # Serve static assets (JS, CSS, images) under /static
 app.mount("/static", StaticFiles(directory="frontend"), name="static")
 
+
+# DEV: disbables caching for static files to ensure latest versions are always loaded
+@app.middleware("http")
+async def no_cache_static(request, call_next):
+    response = await call_next(request)
+    if request.url.path.startswith("/static/"):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "0"
+    return response
+
+# PROD: enables caching for static files to improve performance, redownload every 24 hours
+# @app.middleware("http")
+# async def cache_static_daily(request, call_next):
+#     response = await call_next(request)
+#     if request.url.path.startswith("/static/"):
+#         # Cache for 1 day (86400 seconds)
+#         response.headers["Cache-Control"] = "public, max-age=86400"
+#     return response
+
+
 # Serve index.html at root
 @app.get("/")
 def serve_index():
